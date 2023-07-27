7dfb00f6704132e1d58cb6b948a9fbd258bdadaa
Catherine Cai <83183182+csquareds@users.noreply.github.com>
2023-07-27 12:26:06 -0400

    Update surface area.py

--- surface area.py
+++ surface area.py
@@ -15,7 +15,7 @@ def conc(C1,u1,C2,u2,V2):
     V1 = c2 * V2 / c1
     return V1*1000 # volume in µL
 
-def surfaceArea(value,d,stock,c): # value - conc (mg), d - diameter (nm), stock conc, final conc; for 1000 µL / 1 mL
+def surfaceArea(value,d,stock,c): # particles per mL, d - diameter (nm), stock conc (mg), final conc (mg); for 1000 µL / 1 mL
     volume = conc(stock,'mg',c,'mg',1)
     total = 1000
     dilute = volume / total
@@ -24,7 +24,7 @@ def surfaceArea(value,d,stock,c): # value - conc (mg), d - diameter (nm), stock 
     SA = 4 * π * (diameter/2)**2 # 4πr^2
     return particles*SA
 
-def SA250(value,d,stock,c): # value mg, d in nm; for 250 µL
+def SA250(value,d,stock,c): # particles per mL, d - diameter (nm), stock conc (mg), final conc (mg); for 250 µL
     volume = conc(stock,'mg',c,'mg',0.25)
     total = 250
     dilute = volume / total
